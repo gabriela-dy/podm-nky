85a0802ddaeba2a5e1ff82388ae17d204d9e1dac
Gabriela Dyworová <gabriela.dyworova@gmail.com>
2022-11-08 13:01:05 +0100

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿int a = 0; // do a si přiřadíme na začátku 0
+
+if (a == 0) // pokud je a 0, dáme do něj jedničku
+    a = 1;
+if (a == 1) // pokud je a 1, dáme do něj nulu
+    a = 0;
+
+Console.WriteLine(a);
+Console.ReadKey();
